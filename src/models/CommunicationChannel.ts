defab01498da4d26837eb545a3a332a551071e21
Artur Mędrygał <info@heseya.com>
2021-09-30 11:12:55 +0200

    Improved CommunicationChannel & tests

--- src/models/CommunicationChannel.ts
+++ src/models/CommunicationChannel.ts
@@ -1,20 +1,67 @@
 type EventCallback<Payload = any> = (payload: Payload) => void
 
+interface EventListener {
+  event: string
+  usages: number
+  callback: EventCallback
+}
+
 export class CommunicationChannel {
   name: string
-  private listeners: Record<string, EventCallback[]> = {}
+  private listeners: EventListener[] = []
+  private timeout: number
 
-  constructor(channelName: string) {
+  constructor(channelName: string, timeout = 2000) {
     this.name = channelName
+    this.timeout = timeout
   }
 
+  /**
+   * Allows listening to an event every time it gets emitted
+   */
   on<Payload>(event: string, callback: EventCallback<Payload>): void {
-    if (!this.listeners[event]) this.listeners[event] = []
-
-    this.listeners[event].push(callback)
+    this.listeners.push({ event, callback, usages: Infinity })
   }
 
+  /**
+   * Allows listening to an event only once.
+   */
+  once<Payload>(event: string, callback: EventCallback<Payload>): void {
+    this.listeners.push({ event, callback, usages: 1 })
+  }
+
+  /**
+   * Emits an event to all listeners that are listening to it.
+   */
   emit<T = unknown>(event: string, payload?: T): void {
-    this.listeners[event]?.forEach((callback) => callback(payload))
+    const eventListeners = this.listeners.filter((l) => l.event === event)
+
+    eventListeners.forEach((listener) => {
+      this.emit(`re:${listener.event}`, listener.callback(payload))
+
+      return { ...listener, usages: listener.usages - 1 }
+    })
+
+    this.listeners = this.listeners
+      .map((listener) => {
+        return eventListeners.find((l) => l === listener)
+          ? { ...listener, usages: listener.usages - 1 }
+          : listener
+      })
+      .filter((l) => l.usages > 0)
+  }
+
+  /**
+   * Emits an event, and wait for the response to it
+   */
+  async request<T = unknown>(event: string, payload?: unknown): Promise<T> {
+    return new Promise((resolve, reject) => {
+      this.once<T>(`re:${event}`, (response) => resolve(response))
+      this.emit(event, payload)
+
+      setTimeout(() => {
+        reject(new Error(`Timeout while waiting for response to ${event}`))
+      }, this.timeout)
+    })
   }
 }
---
+++ src/models/tests/CommunicationChannel.test.ts
@@ -0,0 +1,90 @@
+import { CommunicationChannel } from '../CommunicationChannel'
+
+describe('', () => {
+  it('Should create new empty channel', () => {
+    const channel = new CommunicationChannel('test')
+    expect(channel.name).toBe('test')
+    expect((channel as any).listeners.length).toBe(0)
+  })
+
+  it('Should add listener', () => {
+    const channel = new CommunicationChannel('test')
+    const callback = jest.fn()
+    channel.on('event', callback)
+    expect((channel as any).listeners.length).toBe(1)
+
+    const listener = (channel as any).listeners[0]
+    expect(listener.callback).toBe(callback)
+    expect(listener.event).toBe('event')
+    expect(listener.usages).toBe(Infinity)
+  })
+
+  it('Should add single-use listener', () => {
+    const channel = new CommunicationChannel('test')
+    const callback = jest.fn()
+    channel.once('event', callback)
+    expect((channel as any).listeners.length).toBe(1)
+
+    const listener = (channel as any).listeners[0]
+    expect(listener.callback).toBe(callback)
+    expect(listener.event).toBe('event')
+    expect(listener.usages).toBe(1)
+  })
+
+  it('Should emit an event to the given event listeners', () => {
+    const channel = new CommunicationChannel('test')
+
+    const callback = jest.fn((data) => {
+      expect(data).toBe('payload')
+    })
+
+    const callback2 = jest.fn()
+
+    channel.on<string>('event', callback)
+    channel.on<string>('event2', callback2)
+    channel.emit('event', 'payload')
+
+    expect((channel as any).listeners.length).toBe(2)
+
+    expect(callback).toBeCalled()
+    expect(callback2).not.toBeCalled()
+  })
+
+  it('Should remove single-use listeners after emit', () => {
+    const channel = new CommunicationChannel('test')
+
+    const callback = jest.fn((data) => {
+      expect(data).toBe('payload')
+    })
+
+    channel.once<string>('event', callback)
+
+    expect((channel as any).listeners.length).toBe(1)
+
+    channel.emit('event', 'payload')
+
+    expect((channel as any).listeners.length).toBe(0)
+    expect(callback).toBeCalled()
+  })
+
+  it('Channel request should receive a response', async () => {
+    const channel = new CommunicationChannel('test')
+
+    channel.on('reqEvent', () => true)
+
+    const response = await channel.request<boolean>('reqEvent')
+    expect(response).toBe(true)
+  })
+
+  it('Channel request timeout ongoing request when no response', async () => {
+    const channel = new CommunicationChannel('test')
+
+    try {
+      await channel.request('req')
+      // Should not reach this point
+      expect(false).toBe(true)
+    } catch (e) {
+      expect(e).toBeTruthy()
+    }
+  })
+})
